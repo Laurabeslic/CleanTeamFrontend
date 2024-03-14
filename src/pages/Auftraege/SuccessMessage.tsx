8056533b3a03cf87a85da28d74b9fea66be415b6
akt20212 <akt20212@spengergasse.at>
2024-03-14 11:46:40 +0100

    success message hinzugefügt

--- src/pages/Auftraege/SuccessMessage.tsx
+++ src/pages/Auftraege/SuccessMessage.tsx
@@ -7,8 +7,11 @@ interface SuccessMessageProps {
     onHide: () => void;
     art: string;
   }
+  
 
   const SuccessMessage: React.FC<SuccessMessageProps> = ({ show, onHide, art }) => {
+
+    
   useEffect(() => {
     if (show) {
       const timeout = setTimeout(() => {
@@ -19,12 +22,48 @@ interface SuccessMessageProps {
     }
   }, [show, onHide]);
 
+  
+const LogoutIcon = () => {
+  return (
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 130.2 130.2"
+      style={{ width: "40px", height: "40px" }}
+    >
+      <circle
+        className="path circle"
+        fill="none"
+        stroke="#4bd396"
+        strokeWidth="6"
+        strokeMiterlimit="10"
+        cx="65.1"
+        cy="65.1"
+        r="62.1"
+      />
+      <polyline
+        className="path check"
+        fill="none"
+        stroke="#4bd396"
+        strokeWidth="6"
+        strokeLinecap="round"
+        strokeMiterlimit="10"
+        points="100.2,40.2 51.5,88.8 29.8,67.5 "
+      />
+    </svg>
+  );
+};
+
   return (
     <>
-      {show && (
+        {show && (
         <div className="position-fixed bottom-0 start-50 translate-middle-x mb-3">
-          <Alert variant="success" onClose={onHide} >
-            {art} erfolgreich erstellt
+           <div className="logout-checkmark" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <LogoutIcon/>
+          </div>
+          <Alert variant="success" onClose={onHide} className="d-flex align-items-center" style={{maxHeight: '60px'}}>
+           
+            {art} erfolgreich hinzugefügt
           </Alert>
         </div>
       )}
